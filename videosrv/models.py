9daac32bae315dd30291f724d67f3104b254aad5
alexum <alexum2013@yandex.ru>
2022-10-03 19:17:09 +0300

    Первый вариант моделей

--- videosrv/models.py
+++ videosrv/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    subscriptions = models.ManyToManyField('self', symmetrical=False)
+    ignored_users = models.ManyToManyField('self', symmetrical=False)
+
+
+class Video(models.Model):
+    upload = models.FileField(upload_to='/uploads')
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    date_added = models.DateTimeField(auto_now_add=True)
+    header = models.CharField(max_length=255)
+    description = models.TextField()
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
+    text = models.CharField(max_length=512)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+
+class Like(models.Model):
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+
+
+
