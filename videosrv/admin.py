ce63100f34c66b9094f4e44a1c7953695086dd93
alexum <alexum2013@yandex.ru>
2022-10-04 19:09:16 +0300

    Добавлен django-filters

--- videosrv/admin.py
+++ videosrv/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Profile, Video, Comment, Like
+
 
 # Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    pass
