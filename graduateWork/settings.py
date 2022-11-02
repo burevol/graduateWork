06cebf67c0086ea82f9ba09b8877efbf36e89304
alexum <alexum2013@yandex.ru>
2022-11-02 18:06:34 +0300

    Добавлены настройки электронной почты

--- graduateWork/settings.py
+++ graduateWork/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -155,3 +160,12 @@ CHANNEL_LAYERS = {
 
 MEDIA_URL = '/uploads/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+EMAIL_HOST_USER = 'notification@silicon-chronicles.ru'
+EMAIL_HOST_PASSWORD = env('EMAIL_PASSWORD')
